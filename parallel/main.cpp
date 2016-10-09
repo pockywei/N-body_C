bd1887de83f3a7a174927d91f9c69124d7082f0d
Your Name <WEI>
2016-10-09 21:31:42 +1100

    Initial Commit

---
+++ parallel/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  parallel
+//
+//  Created by WEI on 09/10/2016.
+//  Copyright © 2016 WEI. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
